7f7c893607f7fb26b3b27ddf9ea56bfac5d8cc71
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2025-02-17 21:03:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ port = int(os.environ.get("PORT", 8050))
 
 # Lancer l'application
 if __name__ == '__main__':
-    app.run_server(debug=True, port=port)
+    app.run_server(debug=False, host="0.0.0.0", port=port)

19ce0df73b7a3c83376dc5765efa2f0ba4b9bcf8
mathildetissandier <102798509+mathildetissandier@users.noreply.github.com>
2025-02-17 16:55:17 +0100

    Ton message de commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import './App.css';
+// Import des pages (composants) pour chaque route
+import LayoutHome from './components/LayoutHome';
+import LayoutLogement from './components/LayoutLogement';
+import LayoutSante from './components/LayoutSante';
+import LayoutPredictions from './components/LayoutPredictions';
+import LayoutTransports from './components/LayoutTransports';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" style={{ backgroundColor: 'black', color: 'white' }}>
+      <Router>
+        <Switch>
+          {/* Définition des routes et des composants associés */}
+          <Route path="/" exact component={LayoutHome} />
+          <Route path="/logement" component={LayoutLogement} />
+          <Route path="/sante" component={LayoutSante} />
+          <Route path="/predictions" component={LayoutPredictions} />
+          <Route path="/transports" component={LayoutTransports} />
+        </Switch>
+      </Router>
     </div>
   );
 }

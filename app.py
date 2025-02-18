cc1460dc8f9ba7ca993aa19eb22294da81194caa
mathildetissandier <102798509+mathildetissandier@users.noreply.github.com>
2025-02-18 08:59:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,33 +6,27 @@ from flask import Flask
 from layout import layout_home, layout_logement, layout_sante, layout_predictions
 # Assurez-vous d'importer la variable 'layout' définie dans layout_transports
 from layout.layout_transports import layout
-import os
 
-
-app = dash.Dash(__name__, external_stylesheets=[
+# Initialisation de l'application Dash avec un thème Bootstrap
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server, external_stylesheets=[
                 dbc.themes.FLATLY], suppress_callback_exceptions=True)
-server = app.server
+#server = app.server
 
 # Mise en page avec navigation
-
-
 def serve_layout():
     return html.Div(
-        # Fond noir, texte en blanc
-        style={'backgroundColor': 'black', 'color': 'white'},
+        style={'backgroundColor': 'black', 'color': 'white'},  # Fond noir, texte en blanc
         children=[
             dcc.Location(id='url', refresh=False),
             dbc.Container(id='page-content', fluid=True)
         ]
     )
 
-
 app.layout = serve_layout
 layout_sante.register_callbacks(app)
 layout_predictions.register_callbacks(app)
 # Callback pour gérer la navigation
-
-
 @app.callback(
     Output('page-content', 'children'),
     [Input('url', 'pathname')]
@@ -49,10 +43,6 @@ def display_page(pathname):
     else:
         return layout_home.layout
 
-
 # Lancer l'application
-
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 8050)
-    print(f"Starting app on port {port}")  # Log de débogage
-    app.run_server(debug=True, host='0.0.0.0', port=port)
+    app.run_server(debug=True)
